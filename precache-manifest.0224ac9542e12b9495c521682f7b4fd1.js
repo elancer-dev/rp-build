2f9a5f13c8ee91eac5002182fab3edb965484ddd
elancer-dev <mail@localhost>
2020-09-14 10:32:58 +0300

    0.0.1

---
+++ precache-manifest.0224ac9542e12b9495c521682f7b4fd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81f2c32b39fde4d62cdaa4cd71cbc1b",
+    "url": "/rp-build/index.html"
+  },
+  {
+    "revision": "4025f9d0e24a81fdac15",
+    "url": "/rp-build/static/css/main.68b2e402.chunk.css"
+  },
+  {
+    "revision": "189fa145ca300080e602",
+    "url": "/rp-build/static/js/2.3f266ebe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rp-build/static/js/2.3f266ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4025f9d0e24a81fdac15",
+    "url": "/rp-build/static/js/main.7b492b37.chunk.js"
+  },
+  {
+    "revision": "83ff81f2fdeffc4c1ff3",
+    "url": "/rp-build/static/js/runtime-main.457667ed.js"
+  }
+]);
